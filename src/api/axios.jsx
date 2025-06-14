54edd6a5b2a9122aee71b72b4bfbd846d99c87fc
tricrck <tricrck@gmail.com>
2025-06-14 13:13:15 +0300

    UI Change test 3

--- src/api/axios.jsx
+++ src/api/axios.jsx
@@ -3,9 +3,8 @@ import store from '../store'; // Import your Redux store
 import { logout } from '../actions/userActions';
 
 const api = axios.create({
-  baseURL: 'https://money-manager-backend-iczs.onrender.com/api',
+  baseURL: import.meta.env.VITE_BASE_URL,
 });
-
 // Add a request interceptor to include the auth token
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('x-auth-token');
